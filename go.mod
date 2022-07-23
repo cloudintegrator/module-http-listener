64619402ec6e60c4a48000dd341c1fb785ecb2fd
Anupam Gogoi <anupamgogoi0907@gmail.com>
2022-07-23 09:19:28 -0300

    Iitial code added.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cloudintegrator/module-http-listener
+
+go 1.18
+
+require github.com/cloudintegrator/cloud-integrator v0.0.0-20220723121229-d967119797aa // indirect
---
+++ pkg/cloudintegrator/httplistener.go
@@ -0,0 +1,15 @@
+package cloudintegrator
+
+import (
+	"context"
+	"fmt"
+	"github.com/cloudintegrator/cloud-integrator/pkg/listener/spec"
+)
+
+type HttpListener struct {
+	Metadata spec.Listener
+}
+
+func (listener *HttpListener) Start(ctx *context.Context) {
+	fmt.Println("Listener has started listening...")
+}
